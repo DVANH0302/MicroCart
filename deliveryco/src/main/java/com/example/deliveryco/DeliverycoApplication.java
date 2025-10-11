e3bb85c09e36e72d316cf05c584b62420a9388aa
wshi9637 <wshi9637@uni.sydney.edu.au>
2025-10-11 17:02:49 +1100

    Intialised all services, their docker containers, and configured ports to avoid collision

---
+++ deliveryco/src/main/java/com/example/deliveryco/DeliverycoApplication.java
@@ -0,0 +1,13 @@
+package com.example.deliveryco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliverycoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliverycoApplication.class, args);
+	}
+
+}
---
+++ deliveryco/src/test/java/com/example/deliveryco/DeliverycoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deliveryco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliverycoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
